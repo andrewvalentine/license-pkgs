215b5cde780a5f31343593e299ca324bcc98f4e3
Andrew Valentine <andrew.r.valentine@gmail.com>
2017-02-18 11:52:20 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+A repository for storing source files for building license pkgs of macOS
---
+++ mathematicaLicensePkg/buildMathematicaLicensePkg.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Declare your variables here
+
+VERSION=2016
+IDENTIFIER=your.org.mathematica-license
+PKGNAME=Your-Org-Mathemtica-license-$VERSION.pkg
+
+# Stop editing
+
+/usr/bin/pkgbuild --root Payload \
+    --install-location /Library/Mathematica/Licensing \
+    --version $VERSION \
+    --identifier $IDENTIFIER \
+    $PKGNAME
+
+exit 0
